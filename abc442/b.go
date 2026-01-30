caa5bee3482d97579e0ac9160b75eac595a142ef
fuku <fukuyuu0729@gmail.com>
2026-01-30 12:45:04 +0900

    refactor:abc442 a

--- abc442/b.go
+++ abc442/b.go
@@ -6,20 +6,16 @@ import (
 
 func main() {
 	var q int
-	var aArr []int
+	var vol int = 0
+	var play bool = false
+	
 	fmt.Scan(&q)
 
 	for i := 0; i <q; i++ {
 		var a int
 		fmt.Scan(&a)
-		aArr = append(aArr, a)
-	}
-
-
-	var vol int = 0
-	var play bool = false
-	for _, num := range aArr {
-		switch num {
+		
+		switch a {
 		case 1:
 			vol++
 		case 2:
@@ -29,10 +25,10 @@ func main() {
 		case 3:
 			play = !play
 		}
-		if play == true && vol > 2{
+		if play && vol > 2{
 			fmt.Println("Yes")
 		} else {
 			fmt.Println("No")
 		}
-	} 
-}
+	}
+} 
