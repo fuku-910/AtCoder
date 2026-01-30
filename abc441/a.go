810f8df262b2b9304f3a46860e41a6c26ed5a716
fuku <fukuyuu0729@gmail.com>
2026-01-30 13:35:48 +0900

    refactor:abc441 a

--- abc441/a.go
+++ abc441/a.go
@@ -8,10 +8,7 @@ func main() {
 	var p, q, x, y int
 	fmt.Scan(&p, &q, &x, &y)
 
-	p_100 := p + 99
-	q_100 := q + 99
-
-	if p <= x && p_100 >= x && q <= y && q_100 >= y {
+	if p <= x && p + 100 >= x && q <= y && q + 100 >= y {
 		fmt.Println("Yes")
 	} else {
 		fmt.Println("No")
